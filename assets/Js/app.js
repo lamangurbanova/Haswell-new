76dc85c0ff3eb2a6cfde4126391b4bab4b8739b0
lamangurbanova <leman.qurbanova.03@mail.ru>
2023-08-11 15:58:56 +0400

    11.08

---
+++ assets/Js/app.js
@@ -0,0 +1,37 @@
+let cd_search = document.querySelector(".cd-search");
+let search_close = document.querySelector(".search-close");
+let search_btn = document.querySelector(".search-btn");
+let header = document.querySelector(".header");
+let bars = document.querySelector(".bars-btn");
+let accordion = document.querySelector(".accordion");
+
+search_btn.addEventListener("click",()=>{
+    cd_search.style.display ="flex";
+    search_close.style.transform="rotate(-90deg)";
+    search_close.style.transition=".3s ease";
+})
+
+search_close.addEventListener("click",()=>{
+    cd_search.style.display ="none"; 
+})
+
+function rotateElement(){
+    search_close.style.transform="rotate(90deg)";
+}
+
+window.addEventListener("scroll",()=>{
+    let distance = window.scrollY;
+    if(distance>1){
+        header.className="header-scroll"
+        console.log("#e");
+    }
+    else{
+        header.className="header"
+    }
+
+
+})
+
+bars.addEventListener("click",()=>{
+    accordion.classList.toggle("accordion-toggle");
+})
